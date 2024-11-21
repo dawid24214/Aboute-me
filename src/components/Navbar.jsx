ea5a8ccc8dcf8423bab6a445ae42635dae694b83
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-11-21 17:42:32 +0100

    Adding a Responsive Menu with Hamburger
    Created a responsive menu with a hamburger icon and animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,30 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {useState} from "react";
+import {NavLink} from "react-router-dom";
 import '../style/_Navbar.scss';
 
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return(
         <>
             <nav className='navbar'>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About me</Link></li>
-                <li><Link to='/projects'>Projects</Link></li>
-                <li><Link to='/portfolio'>Portfolio</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
+                <div className='logo'>Dawid Grochowski</div>
+                <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+                    <li><NavLink to='/' activeClassName='active' onClick={toggleMenu}>Home</NavLink></li>
+                    <li><NavLink to='/about' activeClassName='active' onClick={toggleMenu}>About me</NavLink></li>
+                    <li><NavLink to='/projects' activeClassName='active' onClick={toggleMenu}>Projects</NavLink></li>
+                    <li><NavLink to='/portfolio' activeClassName='active' onClick={toggleMenu}>Portfolio</NavLink></li>
+                    <li><NavLink to='/contact' activeClassName='active' onClick={toggleMenu}>Contact</NavLink></li>
+                </ul>
             </nav>
 
         </>
-    )
+    );
 }
