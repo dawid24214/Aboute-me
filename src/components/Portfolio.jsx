da78fb54e433c31edeb2abac19a972a36dd1c36a
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-12-02 09:05:21 +0100

    Minimalistic,Rensposive Portfolio
    We created a responsive portfolio with a minimalistic design , featuring a projects section with a unique layout,styled using SCSS

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,9 +4,41 @@ import '../style/_Portfolio.scss';
 export const Portfolio = () =>{
     return (
         <div>
-            <section id='portfolio' className='porfolio'>
-                <h2>Portfolio</h2>
-                <p>Here are some of my best works!</p>
+            <section className="portfolio">
+                <header className="portfolio-header">
+                    <h2>Portfolio</h2>
+                    <p>Overview of My Best Works</p>
+                </header>
+                <div className="portfolio-grid">
+                    <div className="portfolio-item">
+                        <img src="/public/1.jpg" alt="Projekt 1"/>
+                        <div className="portfolio-content">
+                            <h3>Topic 1</h3>
+                            <p>A brief description of the project in a few sentences</p>
+                        </div>
+                    </div>
+                    <div className="portfolio-item">
+                        <img src="/public/1.jpg" alt="Projekt 2"/>
+                        <div className="portfolio-content">
+                            <h3>Topic 2</h3>
+                            <p>A brief description of the project in a few sentences</p>
+                        </div>
+                    </div>
+                    <div className="portfolio-item">
+                        <img src="/public/1.jpg" alt="Projekt 3"/>
+                        <div className="portfolio-content">
+                            <h3>Topic 3</h3>
+                            <p>A brief description of the project in a few sentences</p>
+                        </div>
+                    </div>
+                    <div className="portfolio-item">
+                        <img src="/public/1.jpg" alt="Projekt 4"/>
+                        <div className="portfolio-content">
+                            <h3>Topic 4</h3>
+                            <p>A brief description of the project in a few sentences</p>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     )
